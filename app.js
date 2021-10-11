99101a172577d01d93437bc4b43824de4729bf77
Jim <jnardi1223@gmail.com>
2021-10-11 09:35:07 -0400

    housekeeping

--- app.js
+++ app.js
@@ -9,6 +9,13 @@ const path = require('path');
 const users = require("./routes/api/users");
 const tweets = require("./routes/api/tweets");
 
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('frontend/build'));
+  app.get('/', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
+  })
+}
+
 mongoose
   .connect(db, { useNewUrlParser: true })
   .then(() => console.log("Connected to MongoDB successfully"))
@@ -28,9 +35,3 @@ app.use("/api/tweets", tweets);
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Server is running on port ${port}`));
 
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static('frontend/build'));
-  app.get('/', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
-  })
-}

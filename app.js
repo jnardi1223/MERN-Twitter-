549abfff93166213a8f699aa289945cf20e0c70d
Jim <jnardi1223@gmail.com>
2021-10-11 10:05:05 -0400

    housekeeping

--- app.js
+++ app.js
@@ -1,13 +1,29 @@
-const express = require("express");
+const path = require('path');
+const mongoose = require('mongoose');
+const express = require('express');
 const app = express();
 const db = require('./config/keys').mongoURI;
-const mongoose = require('mongoose');
-const bodyParser = require('body-parser');
-const passport = require('passport');
-const path = require('path');
 
+mongoose
+  .connect(db, { useNewUrlParser: true })
+  .then(() => console.log("Connected to MongoDB successfully"))
+  .catch(err => console.log(err));
+
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+const passport = require('passport');
 const users = require("./routes/api/users");
 const tweets = require("./routes/api/tweets");
+
+app.use(passport.initialize());
+require('./config/passport')(passport);
+app.use("/api/users", users);
+app.use("/api/tweets/", tweets);
+
+const port = process.env.PORT || 5000;
+app.listen(port, () => console.log(`Server is running on port ${port}`));
 
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static('frontend/build'));
@@ -15,23 +31,3 @@ if (process.env.NODE_ENV === 'production') {
     res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
   })
 }
-
-mongoose
-  .connect(db, { useNewUrlParser: true })
-  .then(() => console.log("Connected to MongoDB successfully"))
-  .catch(err => console.log(err));
-
-app.get("/", (req, res) => res.send("Hello World!!"));
-
-app.use(passport.initialize());
-require('./config/passport')(passport);
-
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
-
-app.use("/api/users", users);
-app.use("/api/tweets", tweets);
-
-const port = process.env.PORT || 5000;
-app.listen(port, () => console.log(`Server is running on port ${port}`));
-
